2f02827d85d608ce8b5c5257c3ebde8ea11ef551
Prajwal8705 <75818734+Prajwal8705@users.noreply.github.com>
2020-12-18 14:41:23 -0500

    Trial File
    Does this work

--- Test.java
+++ Test.java
@@ -1,10 +1,21 @@
 public class Test{
-    int [] numbers = new int[3];
+    private int [] numbers = new int[3];
     
     public Test(int [] numbers1){
         numbers = numbers1;
     }
+    public double addNumbers(){
+        double sum = 0;
+        for(int i = 0; i < numbers.length; i++){
+            sum += numbers[i];
+        }
+        return sum;
+    }
     public static void main(String[] args){
+        int[] numbers3 = new int[1];
+        numbers3[0] = 1;
+        Test box = new Test(numbers3);
+        System.out.println(box.addNumbers());
 
     }
 }
